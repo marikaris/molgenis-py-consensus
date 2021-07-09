de0c73a8a7c363a6fdfa98107c1dadd658cf931b
mkslofstra <mariska-karina@hotmail.com>
2021-07-09 09:02:05 +0200

    chore: bump versoin

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='consensus',
-    version='2.0',
+    version='2.1.1',
     packages=find_packages(),
     install_requires=['progressbar2==3.39.3', 'termcolor==1.1.0', 'yaspin==0.14.3', 'pandas==1.2.3'],
     python_requires='>=3.7.1',
